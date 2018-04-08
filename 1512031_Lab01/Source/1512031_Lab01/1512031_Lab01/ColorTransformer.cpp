395301e7b5fb32d025627310794879a8e5b1d3b6
= <=>
2018-04-08 18:35:45 +0700

    xu ly anh

---
+++ 1512031_Lab01/Source/1512031_Lab01/1512031_Lab01/ColorTransformer.cpp
@@ -0,0 +1,442 @@
+﻿#include "stdafx.h"
+#include"ColorTransformer.h"
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+typedef struct RGBColor
+{
+	char R; //Red
+	char G; // green
+	char B; //Blue
+};
+
+bool CheckImageGrayScale(const Mat& imgage)
+{
+	bool checkImageGray = true;//cờ ảnh xám, nếu true là ảnh xám nếu false là ảnh màu. mặc định là true
+							   //kiểm tra ảnh xám hay ảnh màu
+	for (int y = 0; y < imgage.rows; y++) 
+	{
+		for (int x = 0; x < imgage.cols; x++) 
+		{
+			int b = imgage.at<Vec3b>(y, x)[0];
+			int g = imgage.at<Vec3b>(y, x)[1];
+			int r = imgage.at<Vec3b>(y, x)[2];
+			if (!(b == g && b == r))
+			{
+				checkImageGray = false;
+				break;
+			}
+		}
+		if (!checkImageGray) break;
+	}
+	return checkImageGray;
+}
+/*
+-Tạo ma trận ảnh histogram
+biểu đồ dạng cột, nền trắng
+Tham so: 
+	H: mảng histogram một chiều
+	size: kích cỡ biều đồ đầu vào
+	color: màu của cột
+	output: biểu đồ histogram ở dang ảnh vuông (mặc định 255x255)
+Return:
+	1: thành công
+	0: có lỗi
+*/
+int DrawMatHistogram(int *H, Size size, RGBColor color, Mat& output)
+{	
+	if (H == NULL)
+	{
+		return 0;
+	}
+	Mat hist(Size(size.width,size.width), CV_8UC3);
+	//khởi tạo nền trắng
+	for (size_t i = 0; i < size.width; i++)
+	{
+		hist.at<Vec3b>(Point(i, 0))[0] = 255;
+		hist.at<Vec3b>(Point(i, 0))[1] = 255;
+		hist.at<Vec3b>(Point(i, 0))[2] = 255;
+	}
+
+	//vẽ biểu đồ theo màu
+	int p = size.height / size.width + 1;//tỉ lệ cột. p chứa tỉ lệ một pixel của ảnh output bằng p giá trị historgram nhằm giảm tỉ lệ ảnh theo tỉ số p
+	
+	for (size_t i = 0; i < size.width; i++)//duyệt màng H
+	{
+		float high = H[i] / p;
+		for (size_t j = 0; j < high; j++)//vẽ cột trên ảnh output
+		{
+			hist.at<Vec3b>(Point(j, i))[0] = color.B;
+			hist.at<Vec3b>(Point(j, i))[1] = color.G;
+			hist.at<Vec3b>(Point(j, i))[2] = color.R;
+		}
+	}
+	output = hist;
+	return 1;
+}
+//Hàm tính Tần số tích lũy
+void cumhist(int histogram[], int cumhistogram[])
+{
+	cumhistogram[0] = histogram[0];
+
+	for (int i = 1; i < 256; i++)
+	{
+		cumhistogram[i] = histogram[i] + cumhistogram[i - 1];
+	}
+}
+
+int ColorTransformer::ChangeBrighness(const Mat& sourceImage, Mat& destinationImage, uchar b)
+{
+	if (sourceImage.empty())//kiểm tra ảnh có null không
+	{
+		return 0; //anh khong ton tai
+	}
+	destinationImage = sourceImage.clone();//sao chép ảnh đầu vào sang output
+	for (int y = 0; y < sourceImage.rows; y++) 
+	{
+		for (int x = 0; x < sourceImage.cols; x++)
+		{
+			for (int channel = 0; channel < 3; channel++) 
+			{
+				destinationImage.at<Vec3b>(y, x)[channel] = saturate_cast<uchar>//saturate_cast dùng để tránh tràn số cho các kenh màu vì các kênh màu chỉ nhận giá trị trong khoản [0;255]
+					((destinationImage.at<Vec3b>(y, x)[channel]) + b);//tính toán độ sáng mới cho điểm ảnh trên ảnh output
+			}
+		}
+	}
+
+	return 1;//tăng giảm độ sáng thành công
+}
+
+int ColorTransformer::ChangeContrast(const Mat& sourceImage, Mat& destinationImage, float c)
+{
+	if (sourceImage.empty())//kiểm tra ảnh có null không
+	{
+		return 0; //anh khong ton tai
+	}
+	destinationImage = sourceImage.clone();//sao chép ảnh đầu vào sang output
+	for (int y = 0; y < sourceImage.rows; y++) 
+	{
+		for (int x = 0; x < sourceImage.cols; x++)
+		{
+			for (int channel = 0; channel < 3; channel++)
+			{
+				destinationImage.at<Vec3b>(y, x)[channel] = saturate_cast<uchar>//saturate_cast dùng để tránh tràn số cho các kenh màu vì các kênh màu chỉ nhận giá trị trong khoản [0;255]
+					(c*(destinationImage.at<Vec3b>(y, x)[channel]));//tính toán độ sáng mới cho điểm ảnh trên ảnh output
+			}
+		}
+	}
+
+	return 1;//tăng giảm độ sáng thành công
+}
+
+int ColorTransformer::HistogramEqualization(const Mat& sourceImage, Mat& destinationImage)
+{
+	if (sourceImage.empty())//kiểm tra ảnh có null không
+	{
+		return 0; //anh khong ton tai
+	}
+	destinationImage = sourceImage.clone();
+	int histogram[256];
+	// khởi tạo giá trị 0 cho mảng histogram
+	for (int i = 0; i < 256; i++)
+	{
+		histogram[i] = 0;
+	}
+
+	//Tính lược đồ độ xám của ảnh f, lưu vào Hf
+	for (int y = 0; y < destinationImage.rows; y++)
+		for (int x = 0; x < destinationImage.cols; x++)
+			histogram[(int)destinationImage.at<uchar>(y, x)]++;
+	//Tính lược đồ độ xám tích lũy của f, lưu vào T
+	
+	int size = destinationImage.rows * destinationImage.cols;// tính diện tích ảnh
+	float alpha = 255.0 / size;//giá trị alpha : tỉ lệ thu nhỏ biểu đổ Histogram sau khi tinh được, bược này giúp bỏ qua chuẩn hóa giá trị túy lũy
+
+	// Tính toán xác suất của mỗi cường độ điểm ảnh
+	float PrRk[256];
+	for (int i = 0; i < 256; i++)
+	{
+		PrRk[i] = (double)histogram[i] / size;
+	}
+	//Tính lược đồ độ xám tích lũy của f, lưu vào T
+	int T[256];
+	cumhist(histogram, T);
+	
+	int Sk[256];
+	//giảm tỉ lệ tích lũy và chuẩn hóa T về đoạn [0;nG-1] nG=255 lưu vào biến mới Sk 
+	for (int i = 0; i < 256; i++)
+	{
+		Sk[i] = cvRound((double)T[i] * alpha);
+	}
+
+	//Tạo ảnh mới và copy lại ảnh cũ, mục đích
+	Mat new_image = destinationImage.clone();
+
+	for (int y = 0; y < destinationImage.rows; y++)
+		for (int x = 0; x < destinationImage.cols; x++)
+			new_image.at<uchar>(y, x) = saturate_cast<uchar>(Sk[destinationImage.at<uchar>(y, x)]);
+	destinationImage = new_image;//xuất ảnh ra output
+	//free(histogram);
+	return 1;//không tạo được ảnh
+}
+
+int ColorTransformer::CalcHistogram(const Mat& sourceImage, MatNode* histogram)
+{
+	if (sourceImage.empty())//kiểm tra ảnh có null không
+	{
+		return 0; //anh khong ton tai
+	}
+	int imaxWith = 255;//chiều rộng biểu đồ Ouput
+
+	int **H;//khởi tạo mảng histogram, chứa 1 hoặc 3 kênh màu
+			//kiểm tra ảnh xám hay ảnh màu
+	bool isGray = CheckImageGrayScale(sourceImage);
+	if (isGray) // nếu là ảnh xám
+	{
+		int imaxHeight = 0;//chiều cao biểu đồ Histogram kênh Xám Output tạm thời
+		H = new int*[1];
+		H[0] = new int[imaxWith];
+		//khởi tạo giá trị ban đầu là 0
+		for (size_t i = 0; i < imaxWith; i++)
+		{
+			H[0][i] = 0;
+		}
+
+		uchar index;//index của mảng H
+
+		for (int y = 0; y < sourceImage.rows; y++)
+		{
+			for (int x = 0; x < sourceImage.cols; x++)
+			{
+				index = sourceImage.at<uchar>(x, y);//lấy giá trị màu cũng như là index của H
+				H[0][index]++;//tăng histogram lên 1
+				if (H[0][index] > imaxHeight)//kiểm tra imaxHeight có phải là giá trị lớn nhất nữa không
+				{
+					imaxHeight = H[0][index];//gán giá trị max tạm thời
+				}
+			}
+		}
+
+		Mat Histout;
+		RGBColor color;//màu vẽ cột là mầu đen
+		color.B = 0;
+		color.G = 0;
+		color.R = 0;
+		DrawMatHistogram(H[0], Size(imaxWith, imaxHeight), color, Histout);//vẽ histogram ảnh xám
+		histogram->node = Histout;
+		histogram->pnext = NULL;
+	}
+	else // nếu là ảnh màu
+	{
+		int imaxHeightB = 0;//chiều cao biểu đồ Histogram kênh B Output tạm thời
+		int imaxHeightG = 0;//chiều cao biểu đồ Histogram kênh G Output tạm thời
+		int imaxHeightR = 0;//chiều cao biểu đồ Histogram kênh R Output tạm thời
+		H = new int*[3];//3 kênh màu RGB
+		H[0] = new int[imaxWith];//kênh màu B
+		H[1] = new int[imaxWith];//kênh màu G
+		H[2] = new int[imaxWith];//kênh màu R
+								 //khởi tạo giá trị ban đầu là 0
+		for (size_t i = 0; i < imaxWith; i++)
+		{
+			H[0][i] = 0;
+			H[1][i] = 0;
+			H[2][i] = 0;
+		}
+
+		uchar indexB, indexG, indexR;//index của mảng H[0] H[1] H[2]
+
+		for (int y = 0; y < sourceImage.rows; y++)
+		{
+			for (int x = 0; x < sourceImage.cols; x++)
+			{
+				Vec3b color = sourceImage.at<Vec3b>(Point(x, y));
+				//lấy màu của từng kênh
+				indexB = color.val[0];
+				indexG = color.val[1];
+				indexR = color.val[2];
+				//tính histogram
+				H[0][indexB]++;
+				H[1][indexG]++;
+				H[2][indexR]++;
+				if (H[0][indexB] > imaxHeightB)
+				{
+					imaxHeightB = H[0][indexB];
+				}
+				if (H[1][indexG] > imaxHeightG)
+				{
+					imaxHeightG = H[1][indexG];
+				}
+				if (H[2][indexR] > imaxHeightR)
+				{
+					imaxHeightR = H[2][indexR];
+				}
+			}
+		}
+
+		//Tạo biểu đồ Histogram o dang ảnh
+
+		RGBColor colorB; // màu xanh dương
+		colorB.R = 5;
+		colorB.G = 79;
+		colorB.B = 198;
+
+		DrawMatHistogram(H[0], Size(imaxWith, imaxHeightB), colorB, (histogram->node));//tạo sơ đồ ảnh kenh B
+		RGBColor colorG;//màu xanh lá
+		colorG.B = 34;
+		colorG.G = 188;
+		colorG.R = 11;
+		DrawMatHistogram(H[1], Size(imaxWith, imaxHeightG), colorG, histogram->pnext->node);//tạo sơ đồ ảnh kenh G
+		RGBColor colorR;//màu đỏ
+		colorR.B = 4;
+		colorR.G = 4;
+		colorR.R = 232;
+		DrawMatHistogram(H[2], Size(imaxWith, imaxHeightR), colorR, histogram->pnext->pnext->node);//tạo sơ đồ ảnh kenh R
+		
+	}
+
+
+	return 1;//tinh toan histogram thanh cong
+}
+
+int ColorTransformer::CalcHistogram(const Mat& sourceImage, Mat& histogram)
+{
+	if (sourceImage.empty())//kiểm tra ảnh có null không
+	{
+		return 0; //anh khong ton tai
+	}
+	int imaxWith = 255;//chiều rộng biểu đồ Ouput
+	
+	int **H;//khởi tạo mảng histogram, chứa 1 hoặc 3 kênh màu
+	//kiểm tra ảnh xám hay ảnh màu
+	bool isGray = CheckImageGrayScale(sourceImage);
+	if (isGray) // nếu là ảnh xám
+	{
+		int imaxHeight = 0;//chiều cao biểu đồ Histogram kênh Xám Output tạm thời
+		H = new int*[1];
+		H[0] = new int[imaxWith];
+		//khởi tạo giá trị ban đầu là 0
+		for (size_t i = 0; i < imaxWith; i++)
+		{
+			H[0][i] = 0;
+		}
+		
+		uchar index;//index của mảng H
+		
+		for (int y = 0; y < sourceImage.rows; y++)
+		{
+			for (int x = 0; x < sourceImage.cols; x++)
+			{
+				index = sourceImage.at<uchar>(x, y);
+				H[0][index]++;
+				if (H[0][index] > imaxHeight)
+				{
+					imaxHeight = H[0][index];
+				}
+			}
+		}
+		RGBColor color;
+		color.B = 0;
+		color.G = 0;
+		color.R = 0;
+		DrawMatHistogram(H[0], Size(imaxWith, imaxHeight), color, histogram);
+	}
+	else // nếu là ảnh màu
+	{
+		int imaxHeightB = 0;//chiều cao biểu đồ Histogram kênh B Output tạm thời
+		int imaxHeightG = 0;//chiều cao biểu đồ Histogram kênh G Output tạm thời
+		int imaxHeightR = 0;//chiều cao biểu đồ Histogram kênh R Output tạm thời
+		H = new int*[3];//3 kênh màu RGB
+		H[0] = new int[imaxWith];//kênh màu B
+		H[1] = new int[imaxWith];//kênh màu G
+		H[2] = new int[imaxWith];//kênh màu R
+		//khởi tạo giá trị ban đầu là 0
+		for (size_t i = 0; i < imaxWith; i++)
+		{
+			H[0][i] = 0;
+			H[1][i] = 0;
+			H[2][i] = 0;
+		}
+
+		uchar indexB,indexG,indexR;//index của mảng H[0] H[1] H[2]
+
+		for (int y = 0; y < sourceImage.rows; y++)
+		{
+			for (int x = 0; x < sourceImage.cols; x++)
+			{
+				Vec3b color = sourceImage.at<Vec3b>(Point(x, y));
+				//lấy màu của từng kênh
+				indexB = color.val[0];
+				indexG = color.val[1];
+				indexR = color.val[2];
+				//tính histogram
+				H[0][indexB]++;
+				H[1][indexG]++;
+				H[2][indexR]++;
+				if (H[0][indexB] > imaxHeightB)
+				{
+					imaxHeightB = H[0][indexB];
+				}
+				if (H[1][indexG] > imaxHeightG)
+				{
+					imaxHeightG = H[1][indexG];
+				}
+				if (H[2][indexR] > imaxHeightR)
+				{
+					imaxHeightR = H[2][indexR];
+				}
+			}
+		}
+		
+		//Tạo biểu đồ Histogram o dang ảnh
+		//MatNode matnode;
+		Mat HistB;
+		Mat HistG;
+		Mat HistR;
+		//matnode.node = &HistB;
+		//matnode.pnext->node = &HistG;
+
+		RGBColor colorB; // màu xanh dương
+		colorB.R = 5;
+		colorB.G = 79;
+		colorB.B = 198;
+		
+		DrawMatHistogram(H[0], Size(imaxWith, imaxHeightB), colorB, HistB);//tạo sơ đồ ảnh kenh B
+		RGBColor colorG;//màu xanh lá
+		colorG.B = 34;
+		colorG.G = 188;
+		colorG.R = 11;
+		DrawMatHistogram(H[1], Size(imaxWith, imaxHeightG), colorG, HistG);//tạo sơ đồ ảnh kenh G
+		RGBColor colorR;//màu đỏ
+		colorR.B = 4;
+		colorR.G = 4;
+		colorR.R = 232;
+		DrawMatHistogram(H[2], Size(imaxWith, imaxHeightR), colorR, HistR);//tạo sơ đồ ảnh kenh R
+		histogram = HistB;
+	}
+
+
+	return 1;//tinh toan histogram thanh cong
+}
+
+int ColorTransformer::DrawHistogram(const Mat& sourceImage, Mat& histImage)
+{
+	//namedWindow("Histogram gray",CV_WINDOW_AUTOSIZE);
+	CalcHistogram(sourceImage, histImage);
+	//imshow("Histogram gray", histImage);//hiển thị ảnh lên cửa sổ “Show Image”
+	return 0;
+}
+
+int ColorTransformer::DrawHistogram(const Mat& sourceImage, MatNode* histImage)
+{
+	CalcHistogram(sourceImage, histImage);
+	return 0;
+}
+
+ColorTransformer::ColorTransformer()
+{
+
+}
+ColorTransformer::~ColorTransformer()
+{
+
+}
---
+++ 1512031_Lab01/Source/1512031_Lab01/1512031_Lab01/Converter.cpp
@@ -0,0 +1,392 @@
+﻿#include "stdafx.h"
+#include"Converter.h"
+
+int Converter::RGB2GrayScale(const Mat& sourceImage, Mat& destinationImage)
+{
+	//kiểm tra ảnh đầu vao có null hay không
+	if (sourceImage.empty())
+		return 1;//nếu ảnh null trả về 1
+
+	Mat tmp(sourceImage.size(), CV_8UC1);//tạo một ảnh xám để chứa kết quá tính toán về ảnh xám
+	destinationImage = tmp;//gán kết quả ảnh xám cho biến con trả trả về
+
+	for (int y = 0; y < sourceImage.rows; y++)//duyệt các hàng
+	{
+		for (int x = 0; x < sourceImage.cols; x++)//duyệt các cột 
+		{
+			Vec3b color = sourceImage.at<Vec3b>(Point(x, y));//lấy giá trị RGB tại điểm (x,y)
+			int blue = color.val[0];//lấy giá trị màu Blue
+			int green = color.val[1];//lấy giá trị màu Green
+			int red = color.val[2];//lấy giá trị màu Red
+			//Áp dụng công thức chuyển đổi ảnh xám I(x, y) = (2 * Red(x, y) + 5 * Green(x, y) + 1 * Blue(x, y)) / 8
+			destinationImage.at<uchar>(y, x) = (blue + green*5 + red*2)/8;
+		}
+	}
+	return 0;//thành công
+}
+
+int Converter::GrayScale2RGB(const Mat& sourceImage, Mat& destinationImage)
+{
+	if (sourceImage.empty())
+		return 1;
+
+	Mat tmp(sourceImage.size(), CV_8UC1);
+	destinationImage = Mat(sourceImage.size(),CV_8UC3);
+
+	for (int y = 0; y < sourceImage.rows; y++)
+	{
+		for (int x = 0; x < sourceImage.cols; x++)
+		{
+			//lấy vector màu RGB tại điểm (y,x) của ảnh nguồn
+			Vec3b color = sourceImage.at<Vec3b>(Point(x, y));
+			//lấy giá trị các kênh màu R G B thực ra R = G = B
+			float blue = color.val[0];
+			float green = color.val[1];
+			float red = color.val[2];
+			//khôi phục từng kênh màu
+			//ta có ẩn số (2/8)*red+(5/8)*green+(1/8)*blue = I(x,y)
+			blue = blue / 8;
+			green = (green * 8) / 5;
+			red = red * 4;
+			//gán lại giá trị cho các kenh màu vừa tính được
+			color.val[0] = saturate_cast<uchar>(blue);
+			color.val[1] = saturate_cast<uchar>(green);
+			color.val[2] = saturate_cast<uchar>(red);
+			destinationImage.at<Vec3b>(Point(x, y)) = color;
+		}
+	}
+	return 0;
+}
+
+int Converter::RGB2HSV(const Mat& sourceImage, Mat& destinationImage)
+{
+	if (sourceImage.empty())//kiểm tra ảnh đầu vao có null hay không
+	{
+		return 1;//nếu ảnh null trả về 1
+	}
+	float fR, fG, fB;//lưu tạm hệ màu RGB
+	float fH, fS, fV;//lưu hệ tạm màu HSV
+	const float FLOAT_TO_BYTE = 255.0f;//hằng số 255 ở dạng số thực để sau khi nhân chia giảm bớt sai số tránh mất mát dữ liệu
+	const float BYTE_TO_FLOAT = 1.0f / FLOAT_TO_BYTE;//tỉ số 1/255.
+	
+	IplImage iplimageRGB = sourceImage;//chuyển ảnh Mat sang ảnh IplImage
+	IplImage *imageRGB = &iplimageRGB; //trỏ địa chỉ con trỏ IplImage đến data của tấm ảnh
+									   //vì chúng ta không dùng trực tiếp biến tham trị iplimageRGB được
+	//Tạo một tấm ảnh HSV rỗng
+	IplImage *imageHSV = cvCreateImage(cvGetSize(imageRGB), 8, 3);
+	//kiếm tra tấm ảnh HSV imageHSV có tạo được đúng kích thước, chiều sâu 8 và 3 kênh không
+	if (!imageHSV || imageRGB->depth != 8 || imageRGB->nChannels != 3) 
+	{
+		printf("Lỗi, không tạo một tấm ảnh HSV trống được.\n");
+		exit(1);//nếu không đúng thì thoát chương trình.
+	}
+
+	int h = imageRGB->height;		// số Pixel theo chiều cao.
+	int w = imageRGB->width;		// số Pixel theo chiều rộng.
+	int rowSizeRGB = imageRGB->widthStep;	// kích thước một hàng tính bằng byte của ảnh đầu vào RGB.
+	char *imRGB = imageRGB->imageData;	// vị trí pixel bắt đầu làm việc tại vùng data của ảnh RGB đầu vào.
+	int rowSizeHSV = imageHSV->widthStep;	// kích thước một hàng tính bằng byte của ảnh đầu ra HSV.
+	char *imHSV = imageHSV->imageData;	// vị trí pixel bắt đầu ghi dữ liệu của ảnh HSV output.
+	for (int y = 0; y<h; y++) {
+		for (int x = 0; x<w; x++) {
+			// Lấy giá trị màu RGB của pixel(x,y)
+			uchar *pRGB = (uchar*)(imRGB + y*rowSizeRGB + x * 3);
+			int bB = *(uchar*)(pRGB + 0);	// Blue
+			int bG = *(uchar*)(pRGB + 1);	// Green
+			int bR = *(uchar*)(pRGB + 2);	// Red
+
+			// Chuyển các giá trị RGB màu 8bit kiểu interger sang kiểu float
+			fR = bR * BYTE_TO_FLOAT;
+			fG = bG * BYTE_TO_FLOAT;
+			fB = bB * BYTE_TO_FLOAT;
+
+			// Chuyển giá trị màu RGB sang HSV sử dụng kiểu float trong miền [0.0,1.0]
+			float fDelta;//chứa giá trị biến thiên của |fMax-fMin|
+			float fMin // chứa giá trị màu nhỏ nhất của một thành phần màu RGB của điểm poxel đang xét
+				, fMax;//chứa giá trị màu lớn nhất của một thành phần màu RGB của điểm poxel đang xét
+			int iMax;//lưu tạm giá trị màu cho là lớn nhất
+			// so sánh các thành phần màu RGB của pixel đang xet để tìm ra giá trị màu lớn nhất nhỏ nhất
+			if (bB < bG) 
+			{
+				if (bB < bR) 
+				{
+					fMin = fB;
+					if (bR > bG) 
+					{
+						iMax = bR;
+						fMax = fR;
+					}
+					else 
+					{
+						iMax = bG;
+						fMax = fG;
+					}
+				}
+				else 
+				{
+					fMin = fR;
+					fMax = fG;
+					iMax = bG;
+				}
+			}
+			else 
+			{
+				if (bG < bR) 
+				{
+					fMin = fG;
+					if (bB > bR) 
+					{
+						fMax = fB;
+						iMax = bB;
+					}
+					else 
+					{
+						fMax = fR;
+						iMax = bR;
+					}
+				}
+				else 
+				{
+					fMin = fR;
+					fMax = fB;
+					iMax = bB;
+				}
+			}
+			fDelta = fMax - fMin;//tính độ biến thiên
+			fV = fMax;				//tìm được giá trị độ sáng V
+			if (iMax != 0) 
+			{			
+				//kiểm tra đảm bảo nó không rơi vào điểm đen.
+				fS = fDelta / fMax;		// điểm bão hòa.
+				float ANGLE_TO_UNIT = 1.0f / (6.0f * fDelta);	// dùng để điều chình giá trị H (HSV) nằm trong vùng từ [0.0, 1.0] thôi thay vì từ [0.0, 1.0]
+				if (iMax == bR) //nếu giá trị iMax tạm thời bằng giá trị màu Red (RGB)
+				{		
+					// thì giá trị H sẽ nằm giữa màu vàng và màu đỏ tươi.
+					fH = (fG - fB) * ANGLE_TO_UNIT;
+				}
+				else if (iMax == bG) //nếu giá trị iMax tạm thời bằng giá trị màu Green (RGB)
+				{		
+					//thì giá trị H sẽ nằm giữa màu xanh lục và vàng.
+					fH = (2.0f / 6.0f) + (fB - fR) * ANGLE_TO_UNIT;
+				}
+				else //nếu giá trị iMax tạm thời bằng giá trị màu Blue(RGB)
+				{				
+					//thì giá trị H sẽ nằm giữa màu đỏ tươi và màu lam.
+					fH = (4.0f / 6.0f) + (fR - fG) * ANGLE_TO_UNIT;
+				}
+				// Giải quyết ngoại lệ, trường hợp giá trị H (HSV) không nằm tròng vùng mong đợi thì quay vòng giá trị H bằng cách +/- 1.0 tránh bị lỗi 
+				if (fH < 0.0f) // nếu H (HSV) nhỏ hơn 0.0 tức là bị âm
+					fH += 1.0f;//thì fH cộng thêm 1.0
+				if (fH >= 1.0f)//nếu H(HSV) lớn hơn 1.0
+					fH -= 1.0f; //thì fH trừ đi 1.0
+			}
+			else //ngược lại nếu imax khác 0 thì 
+			{
+				fS = 0;//chọn màu đen cho S (HSV)
+				fH = 0;	//
+			}
+
+			// Chuyển hệ màu HSV kiểu float sang kiểu số nguyên 8 bit
+			int bH = (int)(0.5f + fH * 255.0f);
+			int bS = (int)(0.5f + fS * 255.0f);
+			int bV = (int)(0.5f + fV * 255.0f);
+
+			//Kiểm tra hệ màu HSV sau khi chuyển có bị rơi ra ngoài vùng [0;255] hay không
+			if (bH > 255)//nếu H (HSV) lớn hơn 255 là gán lại 255
+				bH = 255;
+			if (bH < 0)//...tương tự
+				bH = 0;
+			if (bS > 255)
+				bS = 255;
+			if (bS < 0)
+				bS = 0;
+			if (bV > 255)
+				bV = 255;
+			if (bV < 0)
+				bV = 0;
+
+			// gán giá trị màu HSV vừa tìm được bên trên vào điểm tương ứng của tấm ảnh rỗng HSV vừa tạo ở trên
+			uchar *pHSV = (uchar*)(imHSV + y*rowSizeHSV + x * 3);
+			*(pHSV + 0) = bH;		// H component
+			*(pHSV + 1) = bS;		// S component
+			*(pHSV + 2) = bV;		// V component
+		}
+	}
+	destinationImage = cvarrToMat(imageHSV);
+	return 0;
+}
+
+int Converter::HSV2RGB(const Mat& sourceImage, Mat& destinationImage)
+{
+	if (sourceImage.empty())//kiểm tra ảnh đầu vao có null hay không
+	{
+		return 1;//nếu ảnh null trả về 1
+	}
+
+	float fH, fS, fV;//lưu tạm hệ màu RGB
+	float fR, fG, fB;//lưu tạm hệ màu HSV
+
+	const float FLOAT_TO_BYTE = 255.0f;//hằng số 255 ở dạng số thực để sau khi nhân chia giảm bớt sai số tránh mất mát dữ liệu
+	const float BYTE_TO_FLOAT = 1.0f / FLOAT_TO_BYTE;//tỉ số 1/255.
+
+	IplImage iplimageHSV = sourceImage;//chuyển ảnh Mat sang ảnh IplImage
+	IplImage *imageHSV = &iplimageHSV; //trỏ địa chỉ con trỏ IplImage đến data của tấm ảnh
+									   //vì chúng ta không dùng trực tiếp biến tham trị iplimageRGB được
+
+									   
+	IplImage *imageRGB = cvCreateImage(cvGetSize(imageHSV), 8, 3);//Tạo một tấm ảnh RGB rỗng dùng để chứa ảnh đầu ra
+																  
+	if (!imageRGB || imageHSV->depth != 8 || imageHSV->nChannels != 3)//kiếm tra tấm ảnh HSV imageHSV có tạo được đúng kích thước, chiều sâu 8 và 3 kênh không
+	{
+		printf("Lỗi, không tạo một tấm ảnh HSV trống được.\n");
+		exit(1);//nếu không đúng thì thoát chương trình.
+	}
+
+	int h = imageRGB->height;		// số Pixel theo chiều cao.
+	int w = imageRGB->width;		// số Pixel theo chiều rộng.
+	int rowSizeRGB = imageRGB->widthStep;	// kích thước một hàng tính bằng byte của ảnh đầu vào RGB.
+	char *imRGB = imageRGB->imageData;	// vị trí pixel bắt đầu làm việc tại vùng data của ảnh RGB đầu vào.
+	int rowSizeHSV = imageHSV->widthStep;	// kích thước một hàng tính bằng byte của ảnh đầu ra HSV.
+	char *imHSV = imageHSV->imageData;	// vị trí pixel bắt đầu ghi dữ liệu của ảnh HSV output.
+	for (int y = 0; y<h; y++) 
+	{
+		for (int x = 0; x<w; x++)
+		{
+			// Lấy giá trị màu RGB của pixel(x,y)
+			uchar *pHSV = (uchar*)(imHSV + y*rowSizeHSV + x * 3);
+			int bH = *(uchar*)(pHSV + 0);	// H
+			int bS = *(uchar*)(pHSV + 1);	// S
+			int bV = *(uchar*)(pHSV + 2);	// V
+
+			// Chuyển các giá trị HSV màu 8bit kiểu interger sang kiểu float
+			fH = (float)bH * BYTE_TO_FLOAT;
+			fS = (float)bS * BYTE_TO_FLOAT;
+			fV = (float)bV * BYTE_TO_FLOAT;
+
+			// Chuyển giá trị màu HSV sang RGB sử dụng kiểu float trong miền [0.0; 1.0]
+			int iI;
+			float fI, fF, p, q, t;
+
+			if (bS == 0)//nếu giá trị S HSV bằng 0
+			{
+				//giá trị màu RGB gán bằng giá trị mức xám
+				fR = fG = fB = fV;
+			}
+			else
+			{
+				// nếu Hue (HSV) == 1.0, thì quay vòng lại giá trị 0.0
+				if (fH >= 1.0f)
+					fH = 0.0f;
+
+				fH *= 6.0;			// 0 - 5
+				fI = floor(fH);		// lấy phần nguyên của giá trị H (0,1,2,3,4,5 hoặc 6)
+				iI = (int)fH;			//		"		"		"		"
+				fF = fH - fI;			// phần giai thừa của H (0 đến 1)
+
+				p = fV * (1.0f - fS);
+				q = fV * (1.0f - fS * fF);
+				t = fV * (1.0f - fS * (1.0f - fF));
+
+				switch (iI) 
+				{
+				case 0:
+					fR = fV;
+					fG = t;
+					fB = p;
+					break;
+				case 1:
+					fR = q;
+					fG = fV;
+					fB = p;
+					break;
+				case 2:
+					fR = p;
+					fG = fV;
+					fB = t;
+					break;
+				case 3:
+					fR = p;
+					fG = q;
+					fB = fV;
+					break;
+				case 4:
+					fR = t;
+					fG = p;
+					fB = fV;
+					break;
+				default:		// case 5 (hoặc 6):
+					fR = fV;
+					fG = p;
+					fB = q;
+					break;
+				}
+			}
+
+			// Chuyển hệ màu RGB vừa tìm được từ kiểu float về kiếu số nguyên 8 bit
+			int bR = (int)(fR * FLOAT_TO_BYTE);
+			int bG = (int)(fG * FLOAT_TO_BYTE);
+			int bB = (int)(fB * FLOAT_TO_BYTE);
+
+			// Kiểm tra giá trị màu RGB khỏi các ngoại lệ
+			if (bR > 255)
+				bR = 255;
+			if (bR < 0)
+				bR = 0;
+			if (bG > 255)
+				bG = 255;
+			if (bG < 0)
+				bG = 0;
+			if (bB > 255)
+				bB = 255;
+			if (bB < 0)
+				bB = 0;
+
+			// Gán các thành phần RGB vào ảnh đầu ra
+			uchar *pRGB = (uchar*)(imRGB + y*rowSizeRGB + x * 3);
+			*(pRGB + 0) = bB;		// Blue
+			*(pRGB + 1) = bG;		// Gren
+			*(pRGB + 2) = bR;		// Red
+		}
+	}
+	destinationImage = cvarrToMat(imageRGB);//gán địa chỉ ảnh output vào contror output
+	return 0;
+}
+
+int Converter::Convert(Mat& sourceImage, Mat& destinationImage, int type)
+{
+	int res; 
+	Mat tmp(sourceImage.size(), CV_8UC1);
+	switch (type)
+	{
+	case 0://RGB to GrayScale
+		res = RGB2GrayScale(sourceImage, destinationImage);
+		break;
+	case 1://GrayScale to RGB
+		res = GrayScale2RGB(sourceImage, destinationImage); 
+		break;
+	case 2://RGB to HSV
+		res = RGB2HSV(sourceImage, destinationImage);
+		break;
+	case 3://HSV to RGB
+		res = HSV2RGB(sourceImage, destinationImage);
+		break;
+
+	default:
+		return 1;
+		break;
+	}
+	return res;
+}
+int Converter::Convert(IplImage* sourceImage, IplImage* destinationImage, int type)
+{
+	return 0;
+}
+
+Converter::Converter()
+{
+
+}
+Converter::~Converter()
+{
+
+}
